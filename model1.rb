bf6d539357d32168e8f4d2ee4f8ddf8c0d38000a
Peter Viss <peter.c.viss@biola.edu>
2019-05-21 17:26:58 -0400

    Coffee Shop

---
+++ model1.rb
@@ -0,0 +1,37 @@
+class Driver
+
+  @@all = []
+
+  attr_accessor :satisfaction
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    satisfaction = 10
+  end
+
+  def driver_info
+    my_driver = Order.all.select do |id|
+      id.driver == self
+    end
+  end
+
+  def orders
+    self.driver_info.collect do |ord|
+      ord.order_id
+    end
+  end
+
+
+  def customer
+    self.driver_info.collect do |ord|
+      ord.customer.name
+    end
+  end
+
+  def satisfaction
+
+  end
+
+end
--- run.rb
+++ run.rb
@@ -1,1 +1,22 @@
+require 'pry'
+require_relative 'model1.rb'
+require_relative "model2.rb"
+require_relative "model3.rb"
 # require all of you files up here
+customer1 = Customer.new("Sally")
+customer2 = Customer.new("Piggy")
+driver1 = Driver.new("Eli")
+driver2 = Driver.new("Charlie")
+order1 = Order.new(customer1, driver1)
+order2 = Order.new(customer1, driver1)
+order3 = Order.new(customer2, driver2)
+customer1.orders
+customer2.driver
+driver1.orders
+driver2.customer
+customer1.satisfaction(7)
+customer2.satisfaction(4)
+customer1.satisfaction(6)
+customer2.satisfaction(10)
+customer1.satisfaction(9)
+Pry.start
